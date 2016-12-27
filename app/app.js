3f707a8f4d88d2805f85f07f2a8bd6e4f0db3517
keith brooks <kbrook10@gmail.com>
2016-12-27 17:56:38 -0500

    Update code to create express server that accesses json data file and renders speaker details upon root file request from end user

--- app/app.js
+++ app/app.js
@@ -1,14 +1,35 @@
+//<-------Example: How to create server with Express.js ----------->
+//<---------------------------------------------------------------->
 var express = require('express');
 var app = express();
+//This path needs to be relative to the current file...
+var dataFile = require('./data/data.json');
+
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
-  res.send('<h1>Roux Academy Meetups</h1>');
+  var info = '';
+  dataFile.speakers.forEach(function(item){
+    info += `
+      <li>
+        <h2>${item.name}</h2>
+        <p>${item.summary}</h2>
+      </li>
+    `;
+  });
+  res.send(`
+    <h1>Roux Academy Meetups</h1>
+    ${info}
+    `
+  );
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port 3000');
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
 });
 
+//<-------Example: How to create server with Node.js -------------->
+//<---------------------------------------------------------------->
 // var http = require('http');
 //
 // var myServer = http.createServer(function(request, response) {
