ad642dba5f359e5b4ad1b0ddaf86db6d91309849
keith brooks <kbrook10@gmail.com>
2016-12-28 17:02:35 -0500

    Update to create partials to allow segments of code to be reused across application

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -1,26 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+//This creates route for the speakers summary page and declares variables to retrieve JSON data, build an empty array and access the speakers data.speakers object...
 router.get('/speakers', function(req, res) {
-  var info = '';
-  var dataFile = req.app.get('appData');
-  dataFile.speakers.forEach(function(item) {
-    info += `
-    <li>
-      <h2>${item.name}</h2>
-      <img src="/images/speakers/${item.shortname}_tn.jpg" alt="speaker">
-      <p>${item.summary}</p>
-    </li>
-    `;
-  });
-  res.send(`
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-      <h1>Roux Academy Meetups</h1>
-      ${info}
-      <script src="/reload/reload.js"></script>
-  `);
+    var data = req.app.get('appData');
+    var pagePhotos = [];
+    var pageSpeakers = data.speakers;
+
+//This loops through the various speakers and retrieves the artwork...
+    data.speakers.forEach(function(item) {
+      pagePhotos = pagePhotos.concat(item.artwork);
+    });
+//This creates global variables when using the speaker route to access pass variables to each view...
+    res.render('speakers', {
+      pageTitle: 'Speakers',
+      artwork: pagePhotos,
+      speakers: pageSpeakers,
+      pageID: 'speakers'
+    });
+
 });
 
+//This creates the route for all of the speakers in the array and displays their unique content...
 router.get('/speakers/:speakerid', function(req, res) {
   var dataFile = req.app.get('appData');
   var speaker = dataFile.speakers[req.params.speakerid];
