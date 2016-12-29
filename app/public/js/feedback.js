8c98f8ac55e5bc012111873e8c74160308d3006b
keith brooks <kbrook10@gmail.com>
2016-12-29 12:53:08 -0500

    Update code to use the POST method to write form data to the server while using body-parser to format request properly

--- app/public/js/feedback.js
+++ app/public/js/feedback.js
@@ -1,6 +1,17 @@
 $(function() {
   $.getJSON('api', updateFeedback);
 
+//This targets the feedback form and gathers the content on submit to send the data (post) to the hard drive...
+  $('.feedback-form').submit(function(e) {
+      e.preventDefault();
+      $.post('api', {
+          name: $('#feedback-form-name').val(),
+          title: $('#feedback-form-title').val(),
+          message: $('#feedback-form-message').val()
+      }, updateFeedback)
+  })
+
+//This renders the new messages from the Feedback form onto the page...Starting with an empty string and concatenating the bootstrap and loop of item data...
   function updateFeedback(data) {
    var output = '';
    $.each(data,function(key, item) {
@@ -8,7 +19,7 @@ $(function() {
      output += '       <div class="feedback-item media">';
      output += '         <div class="feedback-info media-body">';
      output += '           <div class="feedback-head">';
-     output += '             <div class="feedback-title">' + item.title + '<small class="feedback-name label label-info"></small></div>';
+     output += '             <div class="feedback-title">' + item.title + ' <small class="feedback-name label label-info">' + item.name + '</small></div>';
      output += '           </div>';
      output += '           <div class="feedback-message">' + item.message + '</div>';
      output += '         </div>';
